7470471c8d76546e27f0703b2d306ca187c86d89
Yusuf Usta <46392667+Quiec@users.noreply.github.com>
2019-04-26 17:03:24 +0300

    Create FTP.php

---
+++ FTP.php
@@ -0,0 +1,19 @@
+<?php
+require('class.php');
+@set_time_limit(0);
+@clearstatcache();
+@ini_set('max_execution_time',0);
+@ini_set('output_buffering',0);
+
+$count = $_GET['count'];
+if(isset($count)){
+echo "TurkHackTeam.Org - Quiec Gururla Sunar!";
+echo "Kardeşim Kullanıyorsun Bir Teşekkürü Eksik Etmezsin :)";
+
+echo " -----------------------------";
+
+$i = new instaCreator();
+$i->userCreate($count,"3");
+} else {
+echo "Lütfen Count veriniz. FTP.php?count=10 şeklinde.
+}
